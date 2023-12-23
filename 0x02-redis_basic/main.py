d713f1e1d6664b197b9a7afc4447cdf899da9540
calvincetom <calvincetom@outlook.com>
2023-12-23 13:59:29 +0300

    solve checker error

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -2,10 +2,18 @@
 """
 Main file
 """
+import redis
 
 Cache = __import__('exercise').Cache
 
 cache = Cache()
+
+# data = b"hello"
+# key = cache.store(data)
+# print(key)
+
+# local_redis = redis.Redis()
+# print(local_redis.get(key))
 
 cache.store(b"first")
 print(cache.get(cache.store.__qualname__))
